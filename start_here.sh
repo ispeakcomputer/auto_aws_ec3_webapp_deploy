cb8f2a35e711e0e5ef002e9ba1951e02d496cbd3
Ryan <ryan@ispeakcomputer.com>
2021-05-11 00:16:16 -0600

    start script was brokenfixed it

--- start_here.sh
+++ start_here.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 #run chmod +x start_here.sh to make this file execute
+
+export AWS_SECRET_ACCESS_KEY=
+export AWS_ACCESS_KEY_ID=
+export YOUR_AWS_REGION=us-west-1
 
 source venv/bin/active
 python3 app.py
