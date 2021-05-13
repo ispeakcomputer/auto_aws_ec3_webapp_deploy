0d06b4c41a81b5c338920fcd4189b80d60f49971
Ryan <ryan@ispeakcomputer.com>
2021-05-13 14:14:16 -0600

    start.sh to start_here.sh copy wasn't done correctly. Fixed now

--- start_here.sh
+++ start_here.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#run chmod +x start_here.sh to make this file execute
+# Kill python - it might be keeping the key file wrote open
+# sudo pkill -9  python3
 
 export AWS_SECRET_ACCESS_KEY=
 export AWS_ACCESS_KEY_ID=
 export YOUR_AWS_REGION=us-west-1
 
-source venv/bin/active
+source venv/bin/activate
+
 python3 app.py
 
